722d045b0ef79fa6219349ea99c919f191741471
Lucas Uchôa da Veiga <lucas.veiga@laveiga.com.br>
2022-02-09 16:03:37 -0300

    Activity 8

---
+++ Problem-1008.py
@@ -0,0 +1,9 @@
+
+funcionario = int(input())
+horasTrabalhada = int(input())
+valorPorHora = float(input())
+
+salario = horasTrabalhada * valorPorHora
+
+print("NUMBER =", funcionario)
+print("SALARY = U$ %.2f" % salario)
